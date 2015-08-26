4cfd0be07cee070c7d2bc576dda787dd629842ea
Felipe Santos <fesoliveira@outlook.com>
2015-08-25 23:14:17 -0300

    first commit - project outline

---
+++ TacticalEngineOGL_0.2/main.cpp
@@ -0,0 +1,10 @@
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "SOIL.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "assimp.lib")
+#pragma comment(lib, "glew32.lib")
+
+int main(int argc, char* argv[])
+{
+
+}
